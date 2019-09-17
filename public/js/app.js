36fb0b8cb355118762a2e39263ad3b1b51378133
calope03 <calope03@ucm.es>
2019-09-17 12:45:29 +0200

    Add:añadida ficha de personaje

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 function renderCharacter(character) {
-    document.querySelector(".row").innerHTML += `<div class="col-md-3"><div class="card">
+    document.getElementById("renderHere").innerHTML += `<div class="col-md-3"><div class="card">
         <img class="card-img-top" src="${character.image}" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">${character.name}</h5>
@@ -17,10 +17,18 @@ function renderCharacter(character) {
             </div>
         </div>
         <div class="card-body text-right">
-            <a href="#" class="btn btn-primary">Ver más</a>
+            <a href="index.html?character=${character.id}" class="btn btn-primary">Ver más</a>
         </div>
         </div></div>`;
     console.log("Character:", character);
+}
+
+function checkResults(data){
+    if(data.results){
+        data.results.map(val => renderCharacter(val));
+    }else{
+        console.log("mostrar mensaje de error")
+    }
 }
 
 function ajaxHandler(url, cb) {
@@ -44,12 +52,32 @@ function getUrlVars() {
     return vars;
 }
 
+function renderUniqueCharacter(data){
+    console.log(data);
+    document.getElementById("renderHere").innerHTML = `
+    <div class="col-md-12 text-center">
+        <h2>${data.name}</h2>
+    </div>
+    <div class="col-md-4">
+        <img class="img-fluid" src="${data.image}" alt="Card image cap">
+    </div>
+    <div class="col-md-8 row">
+        <div class="col-md-6 details">Estado: </div><div class="col-md-6 details">${data.status}</div>
+        <div class="col-md-6 details">Especie: </div><div class="col-md-6 details">${data.species}</div>
+        <div class="col-md-6 details">Tipo (subespecie): </div><div class="col-md-6 details">${data.type}</div>
+        <div class="col-md-6 details">Genero: </div><div class="col-md-6 details">${data.gender}</div>
+        <div class="col-md-6 details">Planeta de origen: </div><div class="col-md-6 details">${data.origin.name}</div>
+        <div class="col-md-6 details">Localizacion actual: </div><div class="col-md-6 details">${data.location.name}</div>
+        <div class="col-md-6 details">Episodios: </div><div id="listEpisodes" class="col-md-6 details">------------</div>
+    </div>`;
+}
+
 function initApp(){
     const urlParams = getUrlVars();
-    if(urlParams.hasOwnProperty("q")){//Si tenemos algo que buscar
-
+    if(urlParams.hasOwnProperty("name")){//Si tenemos algo que buscar
+        ajaxHandler(`https://rickandmortyapi.com/api/character/?name=${urlParams["name"]}&status=${urlParams["status"]}`, checkResults);
     }else if(urlParams.hasOwnProperty("character")){//Si tenemos parametro de personaje
-
+        ajaxHandler(`https://rickandmortyapi.com/api/character/${urlParams["character"]}`, renderUniqueCharacter);
     }else{//Si no mostramos todos los personajes
         ajaxHandler("https://rickandmortyapi.com/api/character", function (data) {
             data.results.map(val => renderCharacter(val));
